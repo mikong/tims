f29859732a06b2b2abf2c3682d5180efec70e439
mikong <michaelgalero@gmail.com>
2012-05-19 00:24:58 +0800

    Config compass. Use scss.

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'rubygems'
+require 'compass'
 require 'sinatra'
+
+configure do
+  Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
+end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', charset: 'utf-8'
+  scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+end
 
 get '/' do
   haml :index
