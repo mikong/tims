dc92f1a8c0a150a38ded02b6d51689de11d9338e
mikong <michaelgalero@gmail.com>
2012-05-21 23:02:34 +0800

    Use thin web server.

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ configure do
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
 end
 
+set :server, %w(thin webrick)
+
 get '/stylesheets/:name.css' do
   content_type 'text/css', charset: 'utf-8'
   scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
